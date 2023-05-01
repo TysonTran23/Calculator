b61bedc0ca85bfd97d7754966c791aa464a09416
Tyson Tran <tysontran@Tysons-MBP.hitronhub.home>
2023-05-01 09:53:40 -0700

    fixed how some functions, fixed getting DOM inputs, works more efficiently

--- calculator.js
+++ calculator.js
@@ -27,14 +27,14 @@ const rateInput = document.querySelector('#loan-rate');
 loanAmountInput.value = "0"
 yearsInput.value = "0"
 rateInput.value = "0"
-calculateMonthlyPayment();
+update();
 }
 
 // Get the current values from the UI
 // Update the monthly payment
 function update() {
-getCurrentUIValues();
-updateMonthly();
+const currentUIValues = getCurrentUIValues();
+updateMonthly(calculateMonthlyPayment(currentUIValues));
 
 }
 
@@ -42,13 +42,11 @@ updateMonthly();
 // calculate the monthly payment.  The output should be a string
 // that always has 2 decimal places.
 function calculateMonthlyPayment(values) {
-    // Get input values as an object
-  const uiValues = getCurrentUIValues();
 
     // Grab values from object
-  let princple = uiValues.amount;
-  let interst = uiValues.rate / 12 / 100;
-  let n = -(uiValues.years * 12); //Create N negative
+  let princple = values.amount;
+  let interst = values.rate / 12 / 100;
+  let n = Math.floor(-(values.years * 12)); //Create N negative
   
   //Calculating Monthy Payments
   const monthlyPMT = (princple * interst) / (1 - (1 + interst)**n);
@@ -58,6 +56,6 @@ function calculateMonthlyPayment(values) {
 // Given a string representing the monthly payment value,
 // update the UI to show the value.
 function updateMonthly(monthly) {
-newMonthlyPMT = document.querySelector('#monthly-payment')
-newMonthlyPMT.innerText = calculateMonthlyPayment();
+let newMonthlyPMT = document.querySelector('#monthly-payment')
+newMonthlyPMT.innerText = monthly;
 }
